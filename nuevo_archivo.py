a15154386cf55d84859be6e78938ad4c3fb56faa
Luis Francisco Gómez Pérez <luisfgomezperez@gmail.com>
2024-11-23 11:33:15 -0600

    Practica2

---
+++ nuevo_archivo.py
@@ -0,0 +1,8 @@
+
+Name = int(input("Introduzca su nombre de usuario:"))
+Email = int(input("Introduzca un correo electrónixco:"))
+Pasword1 = int(input("Introduzca una contraseña:"))
+Pasword2 = int(input("Comfirmar contraseña:"))
+
+if Name == 1234:
+    print("Welcome")
